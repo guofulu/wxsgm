9acb0aa1739ab02cd68cc7e45df635fff9b9fdc6
glf <13844286410@163.com>
2019-08-03 00:07:22 +0800

    gfl-8.3

---
+++ wxsgm/src/components/home/banner.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.min.css';
+
+class Banner extends React.Component{
+
+    render(){
+        console.log(555,this.props)
+        return (
+            <div className="swiper-container">
+                <div className="swiper-wrapper">
+                    {
+                        this.props.slide_list.map((v,i)=>{
+                            return (
+                               <div className="swiper-slide" >
+                                   <img style={{width:'100%',height:'100%'}} src={v.image_url} alt="" />
+                                </div> 
+                            )
+                        })
+                    }
+                    
+                </div>
+
+                <div className="swiper-pagination"></div>
+
+            </div>
+        )
+    }
+    componentDidMount(){
+
+        var mySwiper = new Swiper ('.swiper-container', {
+            direction: 'horizontal', // 垂直切换选项
+            loop: true, // 循环模式选项
+            speed:300,//自动播放
+            autoplay : {
+                delay:3000
+              },
+            
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          })  
+    }
+}
+
+export default Banner
--- wxsgm/src/store/actionCreator/home/index.js
+++ wxsgm/src/store/actionCreator/home/index.js
@@ -1,47 +1,84 @@
 import homeType from '../../actionType/home';
 import axios from 'axios';
-
+const upRegisterCoupon = function(payload){
+    return {
+        type:homeType.UP_REGISTER_COUPON,
+        payload
+    }
+}
+const upClassifyHome = function(payload){
+    return {
+        type:homeType.UP_CLASSIFY_HOME,
+        payload
+    }
+}
+const upHotsRecommendList = function(payload){
+    return {
+        type:homeType.UP_HOTS_RECOMMEND_LIST,
+        payload
+    }
+}
+const upFloorShow = function(payload){
+    return {
+        type:homeType.UP_FLOOR_SHOW,
+        payload
+    }
+}
+const upRecommendShow = function(payload){
+    return {
+        type:homeType.UP_RECOMMEND_SHOW,
+        payload
+    }
+}
+const upVipHomeSchedular = function(payload){
+    return {
+        type:homeType.UP_VIP_HOME_SCHEDULAR,
+        payload
+    }
+}
 
 export default {
-    getRegisterCoupon(){
+    getRegisterCoupon(){//获取注册优惠券信息
         return async (dispatch)=>{
-            const data = await axios.get('/juoooAPI/Promotion/coupon/getRegisterCoupon?version=6.0.1&referer=2');
-            console.log(111,data)
+            const {data} = await axios.get('/juoooAPI/Promotion/coupon/getRegisterCoupon?version=6.0.1&referer=2');
+            dispatch(upRegisterCoupon(data))
         }
     },
 
-    getClassifyHome(){
+    getClassifyHome(){//获取分类列表
         return async (dispatch)=>{
-            const data = await axios.get('/juoooAPI/home/index/getClassifyHome?city_id=1&abbreviation=SZ&version=6.0.1&referer=2');
-            console.log(222,data)
+            const {data} = await axios.get('/juoooAPI/home/index/getClassifyHome?city_id=1&abbreviation=SZ&version=6.0.1&referer=2');
+            dispatch(upClassifyHome(data))
         }
     },
 
-    getHotsRecommendList(){
+    getHotsRecommendList(){//获取照片推荐列表
         return async (dispatch)=>{
-            const data = await axios.get('/juoooAPI/home/index/getHotsRecommendList?city_id=1&version=6.0.1&referer=2');
+            const {data} = await axios.get('/juoooAPI/home/index/getHotsRecommendList?city_id=1&version=6.0.1&referer=2');
             console.log(333,data)
+            dispatch(upHotsRecommendList(data))
         }
     },
 
-    getFloorShow(){
+    getFloorShow(){//获取楼层信息
         return async (dispatch)=>{
-            const data = await axios.get('/juoooAPI/home/index/getFloorShow?city_id=1&version=6.0.1&referer=2');
-            console.log(444,data)
+            const {data} = await axios.get('/juoooAPI/home/index/getFloorShow?city_id=1&version=6.0.1&referer=2');
+            dispatch(upFloorShow(data))
         }
     },
 
-    getRecommendShow(){
+    getRecommendShow(){//获取推荐信息列表
         return async (dispatch)=>{
-            const data = await axios.get('/juoooAPI/home/index/getRecommendShow?cityAdd=SZ&page=1&version=6.0.1&referer=2');
-            console.log(555,data)
+            const {data} = await axios.get('/juoooAPI/home/index/getRecommendShow?cityAdd=SZ&page=1&version=6.0.1&referer=2');
+            dispatch(upRecommendShow(data))
+            console.log('getRecommendShow',data)
         }
     },
 
-    getVipHomeSchedular(){
+    getVipHomeSchedular(){//获取vip相关信息列表
         return async (dispatch)=>{
-            const data = await axios.get('/juoooAPI/vip/index/getVipHomeSchedular?version=6.0.1&referer=2');
-            console.log(666,data)
+            const {data} = await axios.get('/juoooAPI/vip/index/getVipHomeSchedular?version=6.0.1&referer=2');
+            dispatch(upVipHomeSchedular(data))
         }
     },
 }
